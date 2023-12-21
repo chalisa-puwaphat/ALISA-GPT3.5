f27e2ba9b300faa95e69dc683cb7b2f5ed3c4ae8
Chalisa Puwaphat <chalisapuwaphat9909@gmail.com>
2023-12-21 20:38:46 +0700

    Update app.py
    generate prompts for elderly only

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request
 import openai
 
-
 app = Flask(__name__)
 
 # Set up OpenAI API credentials
 openai.api_key = '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'
-
 
 # Define the default route to return the index.html file
 @app.route("/")
@@ -17,22 +15,23 @@ def index():
 @app.route("/api", methods=["POST"])
 def api():
     # Get the message from the POST request
-    message = request.json.get("message")
-    # Send the message to OpenAI's API and receive the response
-    
-    
+    user_message = request.json.get("message")
+
+    # Generate a prompt based on the user's input
+    prompt = f"Create a user interface suitable for the elderly for a {user_message} app."
+
+    # Send the modified message to OpenAI's API and receive the response
     completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": message}
-    ]
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
     )
-    if completion.choices[0].message!=None:
-        return completion.choices[0].message
 
-    else :
-        return 'Failed to Generate response!'
-    
+    if 'choices' in completion and completion['choices']:
+        return {"content": completion['choices'][0]['message']['content']}
+    else:
+        return {'content': 'Failed to generate response!'}
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run()
