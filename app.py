0b7881d5e17b85e345dcd1a9854848fc0d89fa43
Chalisa Puwaphat <chalisapuwaphat9909@gmail.com>
2023-12-24 12:47:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import openai
 
 app = Flask(__name__)
+app.secret_key = 'alisa'  # Change this to a secret key for session management
 
-# Replace 'your_username', 'your_password', and 'your_database_name' with your actual MySQL credentials
+# Set up OpenAI API credentials
+openai.api_key = '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'
+
+# Configure SQLAlchemy and Bcrypt
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/alisa_users'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Create a SQLAlchemy instance
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-# Define the User model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -26,7 +29,6 @@ def index():
 # Define the /api route to handle POST requests
 @app.route("/api", methods=["POST"])
 def api():
-    # Get the message, device type, and style from the POST request
     user_message = request.json.get("message")
     device_type = request.json.get("device_type")
     selected_style = request.json.get("style")
@@ -51,46 +53,54 @@ def api():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
-        # Handle sign-up form submission
-        # Extract user input from the form
         username = request.form.get("username")
         email = request.form.get("email")
         password = request.form.get("password")
 
-        # Create a new user and add it to the database
-        new_user = User(username=username, email=email, password=password)
+        # Hash the password before storing it in the database
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        new_user = User(username=username, email=email, password=hashed_password)
+
         db.session.add(new_user)
         db.session.commit()
 
-        # Redirect to the index page for now
-        return redirect(url_for("index"))
+        flash('Account created successfully!', 'success')
+        return redirect(url_for("login"))
 
-    # If it's a GET request, render the sign-up page
     return render_template("signup.html")
 
 # Define the /login route to handle GET and POST requests for login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # Handle login form submission
-        # Extract user input from the form
         username = request.form.get("username")
         password = request.form.get("password")
 
-        # Perform authentication (check username and password against the database)
-        user = User.query.filter_by(username=username, password=password).first()
+        user = User.query.filter_by(username=username).first()
 
-        if user:
-            # Authentication successful, redirect to the index page
+        if user and bcrypt.check_password_hash(user.password, password):
+            # Log in the user and store user data in the session
+            session['user_id'] = user.id
+            session['username'] = user.username
+
+            flash('Login successful!', 'success')
             return redirect(url_for("index"))
         else:
-            # Authentication failed, render the login page with an error message
-            return render_template("login.html", error="Invalid username or password")
+            flash('Invalid username or password', 'danger')
 
-    # If it's a GET request, render the login page
     return render_template("login.html")
 
+# Define the /logout route to handle user logout
+@app.route("/logout")
+def logout():
+    # Clear the session data to log out the user
+    session.clear()
+    flash('You have been logged out', 'info')
+    return redirect(url_for("index"))
+
 if __name__ == '__main__':
+    # Create the database tables before running the app
     with app.app_context():
         db.create_all()
     app.run(debug=True)
