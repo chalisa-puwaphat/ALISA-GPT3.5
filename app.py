ca3497744f969c0532be4cd9180955f99c603f51
Chalisa Puwaphat <chalisapuwaphat9909@gmail.com>
2023-12-24 11:38:19 +0700

    Update app.py
    authentication

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import openai
 
 app = Flask(__name__)
@@ -35,5 +35,41 @@ def api():
     else:
         return {'content': 'Failed to generate response!'}
 
+# Define the /signup route to handle GET and POST requests for sign-up
+# Define the /signup route to handle GET and POST requests for sign-up
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        # Handle sign-up form submission
+        # Extract user input from the form
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # Perform validation and database insertion (not implemented in this example)
+
+        # Redirect to the index page for now
+        return redirect(url_for("index"))
+
+    # If it's a GET request, render the sign-up page
+    return render_template("signup.html")
+
+# Define the /login route to handle GET and POST requests for login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Handle login form submission
+        # Extract user input from the form
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Perform validation and authentication (not implemented in this example)
+
+        # Redirect to the index page for now
+        return redirect(url_for("index"))
+
+    # If it's a GET request, render the login page
+    return render_template("login.html")
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
